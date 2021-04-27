607cab51a494494d6d3add10bdd43c3ea38c2fd1
yemisi567 <51059680+yemisi567@users.noreply.github.com>
2021-04-27 15:55:17 -0700

    Create carousel.js
    javascript code for the carousel

---
+++ js/carousel.js
@@ -0,0 +1,31 @@
+var currentSlide = 0;
+const slides = document.querySelectorAll(".slide")
+const dots = document.querySelectorAll('.dot')
+
+const init = (n) => {
+  slides.forEach((slide) => {
+    slide.style.display = "none"
+    dots.forEach((dot) => {
+      dot.classList.remove("active")
+    })
+  })
+  slides[n].style.display = "block"
+  dots[n].classList.add("active")
+}
+const next = () => {
+    currentSlide >= slides.length - 1 ? currentSlide = 0 : currentSlide++
+    init(currentSlide)
+  }
+  
+  const prev = () => {
+    currentSlide <= 0 ? currentSlide = slides.length - 1 : currentSlide--
+    init(currentSlide)
+  }
+  
+setInterval(() => {
+    next()
+  }, 3000);
+  
+  document.querySelector(".next").addEventListener('click', next)
+  document.querySelector(".prev").addEventListener('click', prev)
+document.addEventListener("DOMContentLoaded", init(currentSlide))
